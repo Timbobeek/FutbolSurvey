ef296cd6350464d180fb5d299f6fe8d1e7b14c18
Tim Goloshchapov <tim.goloschapov@gmail.com>
2023-11-25 22:29:09 -0600

    adding accordion wip

--- README.md
+++ README.md
@@ -8,3 +8,18 @@ When done, big thank you message
 At all times, there is a "see other people response' button, where all the data is kept at (on a separate page)
 
 ! you can only edit/delete your own entry 
+
+---------------------------TO DO LIST------------------------------
+
+-need to create the database to keep all the answers at
+
+-are all questions be displayed or by clicking next button, one at a time?
+
+-Once you sign in, what page do you see? 
+
+-What happens once you answer all of the question and hit submit? 
+
+-need to make a POST that is triggered onClick of submit button
+
+-removal/editing only of items with a specific name (so users can't edit others' entries)
+
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { API_KEY } from "../nasastuff";
 import axios from "axios";
+import './Results.css';
 
 // const Results = () => {
 
@@ -28,28 +29,70 @@ import axios from "axios";
 
 const Results = () => {
 
-    const [data, setData] = useState({})
 
-    useEffect(()=>{
-        axios.get(`https://restcountries.com/v3.1/name/eesti`)
-          .then(res=>{
-            setData(res.data[0])
-            //console.log(res.data)
-          })
-          .catch(err =>{
-            console.log(err)
-          })
-      },[]);
 
-      const {capital, cca2} = data;
-      // i wonna have different types of data in data state
+    // const [data, setData] = useState({})
 
-    return (
-        <div className="welcomeText">
-            The poll results will be posted here!<br></br>
+    // useEffect(()=>{
+    //     axios.get(`https://restcountries.com/v3.1/name/eesti`)
+    //       .then(res=>{
+    //         setData(res.data[0])
+    //         //console.log(res.data)
+    //       })
+    //       .catch(err =>{
+    //         console.log(err)
+    //       })
+    //   },[]);
+
+    //   const {capital, cca2} = data;
+
+                {/* The poll results will be posted here!<br></br>
             {capital}<br></br>
             {cca2}
+            <a href="/">Home</a> */}
+
+            const accordionTitles = document.querySelectorAll(".accordionTitle");
+
+            accordionTitles.forEach((accordionTitle) => {
+              accordionTitle.addEventListener("click", () => {
+                if (accordionTitle.classList.contains("is-open")) {
+                  accordionTitle.classList.remove("is-open");
+                } else {
+                  const accordionTitlesWithIsOpen = document.querySelectorAll(".is-open");
+                  accordionTitlesWithIsOpen.forEach((accordionTitleWithIsOpen) => {
+                    accordionTitleWithIsOpen.classList.remove("is-open");
+                  });
+                  accordionTitle.classList.add("is-open");
+                }
+              });
+            });
+            
+
+    return (
+      
+        <div>
+          <div className="accordionItem">
+            <h2 className="accordionTitle">Tab 1</h2>
+            <div className="accordionContent">
+              <p>Information here</p>
+            </div>
+          </div>
+      
+          <div className="accordionItem">
+            <h2 className="accordionTitle">Tab 2</h2>
+            <div className="accordionContent">
+              <p>Information here</p>
+            </div>
+          </div>
+      
+          <div className="accordionItem">
+            <h2 className="accordionTitle">Tab 3</h2>
+            <div className="accordionContent">
+              <p>Information here</p>
+            </div>
+          </div>
         </div>
+
     )
 }
 
