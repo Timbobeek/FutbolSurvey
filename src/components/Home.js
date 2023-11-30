52d790fdc00358c945cb53c056a33f26e7b7746c
Tim Goloshchapov <tim.goloschapov@gmail.com>
2023-11-30 04:31:38 -0600

    first attempt

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,20 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import './Home.css';
+import Form from "./Form";
+
+//https://www.youtube.com/watch?v=QSBc8bABwE0&t=181s&ab_channel=DaveGray
+//https://github.com/gitdagray/react-multi-step-form/blob/main/src/components/Form.js 
+
 
 const Home = () => {
     const { isAuthenticated} = useAuth0();
     return(
         isAuthenticated && (
           <div className="Home">
-            <header className="Home-header">
-            &#9917; Futbol Survey &#9917;
-              <h2>Who is your favorite player?</h2>
-              <form>
-                <input type="text" id="q1answer" name="fname"/><br/>
-                <input type="submit" value="SUBMIT" className="submitBtn"/>
-              </form> 
-            </header>
-            <a href="/results">Results</a>
+            <Form className="form" />
           </div>
         )
     )
 }
-
-// function Home() {
-
-//     //change css names!!!!!!!!!!!!
-//     return (
-//       <div className="Home">
-//         <header className="Home-header">
-//         &#9917; Futbol Survey &#9917;
-//           <h2>Who is your favorite player?</h2>
-//           <form>
-//             <input type="text" id="q1answer" name="fname"/><br/>
-//             <input type="submit" value="SUBMIT" className="submitBtn"/>
-//           </form> 
-//         </header>
-//       </div>
-//     );
-//   }
 
   export default Home;
