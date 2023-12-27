54304972fd6074e121f54dd1fc60c8b2c3fe67b7
Tim Goloshchapov <tim.goloschapov@gmail.com>
2023-12-27 01:37:32 -0600

    refactored disableNext

--- src/context/FormContext.js
+++ src/context/FormContext.js
@@ -54,72 +54,64 @@ export const FormProvider = ({children})=> {
      (data.amateur === true || data.hs === true || data.acad === true || data.college === true || data.semipro === true || data.pro === true) 
      && page === Object.keys(title).length - 1
 
-    const keyFinder = () => {
-        // look for question number
-        let num = page;
-        // get key from data based on that number 
-        let answ = (Object.keys(data))[num];
-        console.log(page, answ)
-        return answ;
-        // e.g. --> 1, data
-    }
-
-    const canNextPage1 = Object.keys(data)
-        .filter(key => key.startsWith('name'))   //makes sure that key "name" has some value entered,
-        .map(key => data[key])
-        .every(Boolean)
-
     // const canNextPage1 = Object.keys(data)
-    //     .filter(key => key.startsWith(keyFinder()))   
+    //     .filter(key => key.startsWith('name'))   //makes sure that key "name" has some value entered,
     //     .map(key => data[key])
     //     .every(Boolean)
 
-    const canNextPage21 = Object.keys(data)
-        .filter(key => key.startsWith('amateur'))   
-        .map(key => data[key])
-        .every(Boolean)
+    // const canNextPage21 = Object.keys(data)
+    //     .filter(key => key.startsWith('amateur'))   
+    //     .map(key => data[key])
+    //     .every(Boolean)
     
-    const canNextPage22 = Object.keys(data)
-        .filter(key => key.startsWith('hs'))   
-        .map(key => data[key])
-        .every(Boolean)
+    // const canNextPage22 = Object.keys(data)
+    //     .filter(key => key.startsWith('hs'))   
+    //     .map(key => data[key])
+    //     .every(Boolean)
     
-    const canNextPage23 = Object.keys(data)
-        .filter(key => key.startsWith('acad'))   
-        .map(key => data[key])
-        .every(Boolean)
+    // const canNextPage23 = Object.keys(data)
+    //     .filter(key => key.startsWith('acad'))   
+    //     .map(key => data[key])
+    //     .every(Boolean)
 
-    const canNextPage24 = Object.keys(data)
-        .filter(key => key.startsWith('college'))   
-        .map(key => data[key])
-        .every(Boolean)
+    // const canNextPage24 = Object.keys(data)
+    //     .filter(key => key.startsWith('college'))   
+    //     .map(key => data[key])
+    //     .every(Boolean)
 
-    const canNextPage25 = Object.keys(data)
-        .filter(key => key.startsWith('semipro'))   
-        .map(key => data[key])
-        .every(Boolean)
+    // const canNextPage25 = Object.keys(data)
+    //     .filter(key => key.startsWith('semipro'))   
+    //     .map(key => data[key])
+    //     .every(Boolean)
 
-    const canNextPage26 = Object.keys(data)
-        .filter(key => key.startsWith('pro'))   
-        .map(key => data[key])
-        .every(Boolean)
-
-    const canNextPage3 = Object.keys(data)
-        .filter(key => key.startsWith('mr'))   
-        .map(key => data[key])
-        .every(Boolean)
+    // const canNextPage26 = Object.keys(data)
+    //     .filter(key => key.startsWith('pro'))   
+    //     .map(key => data[key])
+    //     .every(Boolean)
 
     // const canNextPage3 = Object.keys(data)
-    //     .filter(key => key.startsWith(keyFinder()))   
+    //     .filter(key => key.startsWith('mr'))   
     //     .map(key => data[key])
     //     .every(Boolean)
 
-    const canNextPage4 = Object.keys(data)
-        .filter(key => key.startsWith('favteam'))
-        .map(key => data[key])
-        .every(Boolean)
+    // // const canNextPage3 = Object.keys(data)
+    // //     .filter(key => key.startsWith(keyFinder()))   
+    // //     .map(key => data[key])
+    // //     .every(Boolean)
+
+    // const canNextPage4 = Object.keys(data)
+    //     .filter(key => key.startsWith('favteam'))
+    //     .map(key => data[key])
+    //     .every(Boolean)
 
     ///////////////////////////////////////////////////////////
+
+    function keysFilled(keys) {
+        return Object.keys(data)
+            .filter(key => keys.includes(key))
+            .map(key => data[key])
+            .some(Boolean)
+    }
 
     const disablePrev = page === 0
 
@@ -127,12 +119,18 @@ export const FormProvider = ({children})=> {
 
     const disableNext =
             (page === Object.keys(title).length - 1)
-            || (page === 0 && !canNextPage1)
-            || (page === 1 && !canNextPage21 && !canNextPage22 && !canNextPage23 && !canNextPage24 && !canNextPage25 && !canNextPage26)
-            || (page === 2 && !canNextPage3)
-            || (page === 3 && !canNextPage4)
+            || (page === 0 && !keysFilled(['name']))
+            || (page === 1 && !keysFilled(['amateur', 'hs', 'acad', 'college', 'semipro', 'pro']))
+            || (page === 2 && !keysFilled(['mr']))
+            || (page === 3 && !keysFilled(['favteam']))
 
-    
+    // const disableNext =
+    //         (page === Object.keys(title).length - 1)
+    //         || (page === 0 && !canNextPage1)
+    //         || (page === 1 && !canNextPage21 && !canNextPage22 && !canNextPage23 && !canNextPage24 && !canNextPage25 && !canNextPage26)
+    //         || (page === 2 && !canNextPage3)
+    //         || (page === 3 && !canNextPage4)
+
     const prevHide = page === 0 && "remove-button"
     
     const nextHide = page === Object.keys(title).length - 1 && "remove-button"
