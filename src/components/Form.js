f92517331e9fdee26f1e2b9a2d63ef258a716745
Tim Goloshchapov <tim.goloschapov@gmail.com>
2024-01-14 01:35:28 -0600

    useEffect for survey images

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,32 @@
+import ronaldo from "../jersey.jpg"
+import img1 from "../surveyImages/vennegor.jpg"
 import { useNavigate } from "react-router-dom"
 import useFormContext from "../hooks/useFormContext"
 import FormInputs from './FormInputs'
 import './Form.css';
+import { useEffect, useState } from "react";
+
+const pageToColor = new Map([
+    [0, "green"],
+    [1, "yellow"],
+    [2, "red"],
+    [3, "blue"]
+])
+
+const pageToImage = new Map([
+    [0, `url(${img1})`],
+    [1, `url(${ronaldo})`],
+])
+
+const defaultColor = "yellow"
+
+const defaultImage = "none";
 
 const Form = () => {
 
     const navigate = useNavigate();
+    const [color, setColor] = useState("yellow")
+    const [image, setImage] = useState("none")
 
     const {
         page,
@@ -29,9 +50,16 @@ const Form = () => {
         console.log(JSON.stringify(data))
     }
 
-    const content = (
-        <form className="formContainer" onSubmit={handleSubmit}>
+    useEffect(() => {
+        const color = pageToColor.get(page) ?? defaultColor
+        setColor(color)
+        const image = pageToImage.get(page) ?? defaultImage
+        console.log(image);
+        setImage(image)
+    }, [page])
 
+    const content = (
+        <form id="formCont" className="formContainer" onSubmit={handleSubmit} style={{backgroundImage: image}}>
             <header>
                 <h2 className="questionTitle">{title[page]}</h2>
                 <div className="button-container">
--- src/components/SurveyOne.js
+++ src/components/SurveyOne.js
@@ -2,8 +2,6 @@ import Form from "./Form";
 import { FormProvider } from "../context/FormContext";
 import { useAuth0 } from "@auth0/auth0-react";
 import './SurveyOne.css'
-//https://www.youtube.com/watch?v=QSBc8bABwE0&t=181s&ab_channel=DaveGray
-//https://github.com/gitdagray/react-multi-step-form/blob/main/src/components/Form.js 
 
 
 const SurveyOne = () => {
