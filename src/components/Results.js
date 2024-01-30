e7f3a3e447ff9769727707e7fc5603da6ba9b2de
Tim Goloshchapov <tim.goloschapov@gmail.com>
2024-01-29 18:56:52 -0600

    levelsPlayed fixed sloppy version

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { API_KEY } from "../nasastuff";
 import axios from "axios";
 import "./Results.css";
+
+const apiKey = process.env.REACT_APP_API_KEY;
 
 // const Results = () => {
 
@@ -54,13 +55,12 @@ const Results = () => {
           url: `https://ferrata-crud2.builtwithdark.com/v1/surveys/`,
           method: 'get',
           headers: {
-            'x-api-key': `${API_KEY}`
+            'x-api-key': `${apiKey}`
           }
         })
           .then(res=>{
             setData(res.data)
             console.log(res.data);
-            console.log(Object.entries(res.data)[0][1].name)
           })
           .catch(err =>{
             console.log(err)
@@ -141,79 +141,128 @@ const Results = () => {
   //    },
   // ];
 
-  const userData = {
-    'obj1': { 
-      // title: "Tab 1",
-      'name': "Jessica Knoxman",
-      "Favorite position": "Midfield",
-      "Favorite player": "Cristiano Ronaldo",
-      "Messi or Ronaldo": "Messi",
-      "Favorite club": "Bayern Munchen",
-      "Favorite national team": "Australia",
-      "Favorite league": "Bundesliga",
-      "Favorite jersey I own": "2007 Barcelona Away Puyol ",
-      "Favorite memory as a specator": "The Invincibles Arsenal Season jsdhfjdshj djshfjkdshf sdhnfjksdhkfjh sdfjhsdjfhjks sdhjfhdsjfhs",
-      "Worst memory as a spectator": "Arsen Wenger leaving Arsenal",
-      "Favorite memory as a player": "Scoring a double in the HS State final",
-      "Worst memory as a player": "Breaking my knee freshman year college",
-      "I started playing at": "7",
-      "I started playing because": "My parents signed me up for a local club",
-      "Levels I played at": "Semi-Pro",
-      "My biggest achievement is": "Winning State with HS",
-      "My futbol-related goal is": "Want to win Kickers A league",
-      "Best futbol advice I have received": "Listen to your heart",
-      "Favorite pair of cleats": "Nike Total 90",
-      "Favorite ball": "Brazuca",
-      "Jabulani is": "Trash",
-      "Best compliment I received": "Your touch is magical"
-    },
-    'obj2': { 
-      // title: "Tab 2",
-      'name': "Abdul Salah",
-      a1: "Goakeeper",
-      a2: "Mo Salah",
-      a3: "Ronaldo",
-      a4: "Al-Nassr",
-      a5: "Saudi Arabia",
-      a6: "Premier League",
-      a7: "2022 Al-Nassr Ronaldo",
-      a8: "2022 WC Saudi Arabia - Argentina 2:1",
-      a9: "2022 WC Saudi Arabia not making it to playoffs",
-      a10: "Saving a penalty last Sunday",
-      a11: "Conceding a goal from 40 yards out",
-      a12: "18",
-      a13: "My dad was an a pro player",
-      a14: "HS",
-      a15: "Winning State with HS",
-      a16: "Want to win Kickers A league",
-      a17: "Listen to your heart",
-      a18: "Love it"
-    },
-    'obj3':{ 
-      // title: "Tab 3",
-      'name': "Ivan Popov",
-      a1: "Forward",
-      a2: "Dimitar Berbatov",
-      a3: "Messi",
-      a4: "Manchester United",
-      a5: "Bulgaria",
-      a6: "Serie A",
-      a7: "2012 Fulham Home Berbatov",
-      a8: "Euro 2004",
-      a9: "Berbatov playing for MU",
-      a10: "Scoring a double in the HS State final",
-      a11: "Breaking my knee freshman year college",
-      a12: "5",
-      a13: "My parents signed me up for a local club",
-      a14: "Semi-Pro",
-      a15: "Winning State with HS",
-      a16: "Want to win Kickers A league",
-      a17: "Listen to your heart",
-      a18: "No idea"
-     },
-  };
+  // const userData = {
+  //   'obj1': { 
+  //     // title: "Tab 1",
+  //     'name': "Jessica Knoxman",
+  //     "Favorite position": "Midfield",
+  //     "Favorite player": "Cristiano Ronaldo",
+  //     "Messi or Ronaldo": "Messi",
+  //     "Favorite club": "Bayern Munchen",
+  //     "Favorite national team": "Australia",
+  //     "Favorite league": "Bundesliga",
+  //     "Favorite jersey I own": "2007 Barcelona Away Puyol ",
+  //     "Favorite memory as a specator": "The Invincibles Arsenal Season jsdhfjdshj djshfjkdshf sdhnfjksdhkfjh sdfjhsdjfhjks sdhjfhdsjfhs",
+  //     "Worst memory as a spectator": "Arsen Wenger leaving Arsenal",
+  //     "Favorite memory as a player": "Scoring a double in the HS State final",
+  //     "Worst memory as a player": "Breaking my knee freshman year college",
+  //     "I started playing at": "7",
+  //     "I started playing because": "My parents signed me up for a local club",
+  //     "Levels I played at": "Semi-Pro",
+  //     "My biggest achievement is": "Winning State with HS",
+  //     "My futbol-related goal is": "Want to win Kickers A league",
+  //     "Best futbol advice I have received": "Listen to your heart",
+  //     "Favorite pair of cleats": "Nike Total 90",
+  //     "Favorite ball": "Brazuca",
+  //     "Jabulani is": "Trash",
+  //     "Best compliment I received": "Your touch is magical"
+  //   },
+  //   'obj2': { 
+  //     // title: "Tab 2",
+  //     'name': "Abdul Salah",
+  //     a1: "Goakeeper",
+  //     a2: "Mo Salah",
+  //     a3: "Ronaldo",
+  //     a4: "Al-Nassr",
+  //     a5: "Saudi Arabia",
+  //     a6: "Premier League",
+  //     a7: "2022 Al-Nassr Ronaldo",
+  //     a8: "2022 WC Saudi Arabia - Argentina 2:1",
+  //     a9: "2022 WC Saudi Arabia not making it to playoffs",
+  //     a10: "Saving a penalty last Sunday",
+  //     a11: "Conceding a goal from 40 yards out",
+  //     a12: "18",
+  //     a13: "My dad was an a pro player",
+  //     a14: "HS",
+  //     a15: "Winning State with HS",
+  //     a16: "Want to win Kickers A league",
+  //     a17: "Listen to your heart",
+  //     a18: "Love it"
+  //   },
+  //   'obj3':{ 
+  //     // title: "Tab 3",
+  //     'name': "Ivan Popov",
+  //     a1: "Forward",
+  //     a2: "Dimitar Berbatov",
+  //     a3: "Messi",
+  //     a4: "Manchester United",
+  //     a5: "Bulgaria",
+  //     a6: "Serie A",
+  //     a7: "2012 Fulham Home Berbatov",
+  //     a8: "Euro 2004",
+  //     a9: "Berbatov playing for MU",
+  //     a10: "Scoring a double in the HS State final",
+  //     a11: "Breaking my knee freshman year college",
+  //     a12: "5",
+  //     a13: "My parents signed me up for a local club",
+  //     a14: "Semi-Pro",
+  //     a15: "Winning State with HS",
+  //     a16: "Want to win Kickers A league",
+  //     a17: "Listen to your heart",
+  //     a18: "No idea"
+  //    },
+  // };
 
-  const tabs = Object.values(userData);
+  const userDataKeys = [
+      "Favorite position",
+      "Favorite player",
+      "Messi or Ronaldo",
+      "Favorite club",
+      "Favorite national team",
+      "Favorite league",
+      "Favorite jersey I own",
+      "Favorite memory as a spectator",
+      "Worst memory as a spectator",
+      "Favorite memory as a player",
+      "Worst memory as a player",
+      "I started playing at",
+      "I started playing because",
+      "Levels I played at",
+      "My biggest achievement is",
+      "My futbol-related goal is",
+      "Best futbol advice I have received",
+      "Favorite pair of cleats",
+      "Favorite ball",
+      "Jabulani is",
+      "Best compliment I received"
+  ]
+
+  const tabs = Object.values(data);
+
+  // let levelValues = tabs.map(tab => {
+  //   let vals = [];
+  //   if ( (Object.entries(tab).find((elem) => (elem[0] === 'acad' && elem[1] === true))) !== undefined ){
+  //     levelValues.push('Academy');
+  //   }
+  //   if ( (Object.entries(tab).find((elem) => (elem[0] === 'amateur' && elem[1] === true))) !== undefined ){
+  //     levelValues.push('Amateur');
+  //   }
+  //   if ( (Object.entries(tab).find((elem) => (elem[0] === 'college' && elem[1] === true))) !== undefined ){
+  //     levelValues.push('College');
+  //   }
+  //   if ( (Object.entries(tab).find((elem) => (elem[0] === 'hs' && elem[1] === true))) !== undefined ){
+  //     levelValues.push('High School');
+  //   }
+  //   if ( (Object.entries(tab).find((elem) => (elem[0] === 'pro' && elem[1] === true))) !== undefined ){
+  //     levelValues.push('Professional');
+  //   }
+  //   if ( (Object.entries(tab).find((elem) => (elem[0] === 'semipro' && elem[1] === true))) !== undefined ){
+  //     levelValues.push('Semi-professional');
+  //   }
+  //   return vals;
+  // })
+
+  
 
 
   return (
@@ -224,6 +273,7 @@ const Results = () => {
 
       {tabs.map((tab, i) => (
         <div className="accordionItem">
+          
          
           <h2
             id={`accordionTitle-${i}`}
@@ -240,12 +290,51 @@ const Results = () => {
 
 
           <div className="accordionContent">
+            
+            {Object.entries(tab).map((arr,i) => (
 
-            {Object.entries(tab).map((arr) => (
                 <div className={ arr[0] !== 'name' ? "answers" : "answersName"}> 
-                  <p className="answersLeft">{arr[0]} </p>
+
+                  <p className="answersLeft">{userDataKeys[i]} </p>
+                  
                   <div className="spacer"/>
-                  <p className="answersRight">{arr[1]} </p> 
+                  {/* <p className="answersRight">{arr[1]} </p>  */}
+                  {/* {console.log(userDataKeys[i],arr[0],arr[1])} */}
+                  <p className="answersRight">
+                    {
+                    ((userDataKeys[i] !== userDataKeys[13]) ? arr[1] : '')
+                    } 
+                    {
+                    ((userDataKeys[i] === userDataKeys[13]) &&
+                    (((Object.entries(tab).find((elem) => (elem[0] === 'acad' && elem[1] === true))) !== undefined ))) 
+                    ? 'Academy ' : ''
+                    } 
+                    {
+                    ((userDataKeys[i] === userDataKeys[13]) &&
+                    (((Object.entries(tab).find((elem) => (elem[0] === 'amateur' && elem[1] === true))) !== undefined ))) 
+                    ? 'Amateur ' : ''
+                    }
+                    {
+                    ((userDataKeys[i] === userDataKeys[13]) &&
+                    (((Object.entries(tab).find((elem) => (elem[0] === 'college' && elem[1] === true))) !== undefined ))) 
+                    ? 'College ' : ''
+                    }
+                    {
+                    ((userDataKeys[i] === userDataKeys[13]) &&
+                    (((Object.entries(tab).find((elem) => (elem[0] === 'hs' && elem[1] === true))) !== undefined ))) 
+                    ? 'High School ' : ''
+                    }
+                    {
+                    ((userDataKeys[i] === userDataKeys[13]) &&
+                    (((Object.entries(tab).find((elem) => (elem[0] === 'semipro' && elem[1] === true))) !== undefined ))) 
+                    ? 'Semi-Pro ' : ''
+                    }
+                    {
+                    ((userDataKeys[i] === userDataKeys[13]) &&
+                    (((Object.entries(tab).find((elem) => (elem[0] === 'pro' && elem[1] === true))) !== undefined ))) 
+                    ? 'Pro ' : ''
+                    }
+                  </p>
                 </div>
             ))}
 
--- src/nasastuff.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY = 'eb9e6c9c5b16ae86af451f7159989e6d215d3dfb6347bccd9302b9418700b766'
-                       
