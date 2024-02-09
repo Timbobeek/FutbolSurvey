1d732ccf2815e8d635ac57493d5fe19bc78b1c39
Tim Goloshchapov <tim.goloschapov@gmail.com>
2024-02-09 17:46:45 -0600

    survey form css

--- src/components/SurveyForm.js
+++ src/components/SurveyForm.js
@@ -3,6 +3,7 @@ import './Form.css';
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
+import './SurveyForm.css';
 
 
 import loadingImg from "../surveyImages/danburn.jpg"
